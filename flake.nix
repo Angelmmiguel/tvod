b5c4e9282544d4d5a57e32b213096917ec1559d3
Angel M Miguel <hi@angel.kiwi>
2025-12-09 20:15:39 +0100

    feat: create basic project and initial configuration

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs
+  }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [nodejs_22 pnpm bashInteractive];
+    };
+  });
+}
